c1991b3f83761593d7d3ef304cdc19c41e61cf49
RISHANKMISH <rishankm19@gmail.com>
2026-01-22 23:53:25 +0530

    DSA in Java

---
+++ Relook.java
@@ -0,0 +1,18 @@
+class Relook{
+    public static void main(String[] args) {
+//        System.out.print("Hey");
+//
+//        int n;
+//        System.out.println("Enter value of n: ");
+//        Scanner sc= new Scanner(System.in);
+//        n= sc.nextInt();
+//        System.out.println("VALUE OF N: " +n);
+
+
+       int[] arr;
+       arr= {1, 2, 3, 4, 5};
+
+    }
+
+}
+
