1d21c00426ad54d7b8e9dc6f6c8625011754118a
RISHANKMISH <rishankm19@gmail.com>
2026-02-04 23:03:03 +0530

    Order Agnostic Binary Search

--- Arrays/OrderAgnosticBinarySearch.java
+++ Arrays/OrderAgnosticBinarySearch.java
@@ -1,5 +1,44 @@
-import java.util.*;
-
 public class OrderAgnosticBinarySearch {
-    
+
+    public static void main(String[] args) {
+
+        int[] arr = {98, 95, 54, 32, 12, 9};
+        int kkey = 9;
+
+        int result = DifferentOrderBinarySearch(arr, kkey);
+        System.out.println(result);
+    }
+
+    static int DifferentOrderBinarySearch(int[] arr, int kkey) {
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        boolean Asc = arr[start] < arr[end];
+
+        while (start <= end) {
+
+            int middle = start + (end - start) / 2;
+
+            if (arr[middle] == kkey) {
+                return middle;
+            }
+
+            if (Asc) {
+                if (kkey < arr[middle]) {
+
+                    end = middle - 1;
+                } else {
+                    start = middle + 1;
+                }
+            } else {
+                if (kkey > arr[middle]) {
+                    end = middle - 1;
+                } else {
+                    start = middle + 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
